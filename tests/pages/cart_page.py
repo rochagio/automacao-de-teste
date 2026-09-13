90e64302b6f0714537191b199a0797ac96ea5f3d
Giovane Rocha <giovane@DESKTOP-IP36AVP.localdomain>
2026-09-12 23:18:33 -0300

    Add ajustesete

--- tests/pages/cart_page.py
+++ tests/pages/cart_page.py
@@ -1,4 +1,7 @@
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 from .base_page import BasePage
 
 
@@ -7,4 +10,12 @@ class CartPage(BasePage):
     CHECKOUT = (By.ID, "checkout")
 
     def start_checkout(self):
-        self.click(*self.CHECKOUT)
+        WebDriverWait(self.driver, 10).until(
+            EC.url_contains("/cart.html")
+        )
+
+        checkout = WebDriverWait(self.driver, 10).until(
+            EC.element_to_be_clickable(self.CHECKOUT)
+        )
+
+        checkout.click()
