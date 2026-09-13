5fa2afbe04383f5f6e881157753c8149a236296e
Giovane Rocha <giovane@DESKTOP-IP36AVP.localdomain>
2026-09-12 23:18:33 -0300

    Add ajustesete

--- tests/pages/base_page.py
+++ tests/pages/base_page.py
@@ -1,15 +1,23 @@
-from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
 class BasePage:
+
     def __init__(self, driver):
         self.driver = driver
+        self.wait = WebDriverWait(driver, 10)
 
     def find(self, by, value):
-        return self.driver.find_element(by, value)
+        return self.wait.until(
+            EC.presence_of_element_located((by, value))
+        )
 
     def click(self, by, value):
-        self.find(by, value).click()
+        element = self.wait.until(
+            EC.element_to_be_clickable((by, value))
+        )
+        element.click()
 
     def type(self, by, value, text):
         self.find(by, value).send_keys(text)
